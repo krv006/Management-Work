c2bbe2330c9517a7d64e0776f9be4cb50407da90
krv006 <rvkamronbek@gmail.com>
2025-03-17 23:21:33 +0500

    change

--- apps/models.py
+++ apps/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import Model, CharField, TextField, ForeignKey, CASCADE, DateTimeField
+from django.db.models import Model, CharField, TextField, ForeignKey, CASCADE, DateTimeField, ManyToManyField
 from django.db.models.fields import IntegerField
 
 
@@ -15,3 +15,20 @@ class Project(Model):
     member_of_project = IntegerField(default=0)
     date_line = CharField(max_length=100, help_text='Date line of your task (1 month): ')
     user = ForeignKey('apps.User', CASCADE, related_name='projects')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"Project name: {self.name}"
+
+
+class Task(Model):
+    name = CharField(max_length=100, help_text='Task name: ')
+    user = ManyToManyField('apps.User')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"task name: {self.name}"
