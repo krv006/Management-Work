f9fb5ac743ab06e1ae621b64fcbc0bfc4f748a22
krv006 <rvkamronbek@gmail.com>
2025-03-17 23:14:15 +0500

    change

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Model, CharField, TextField, ForeignKey, CASCADE, DateTimeField
+from django.db.models.fields import IntegerField
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Project(Model):
+    name = CharField(max_length=100, help_text='Project name: ')
+    task = TextField(help_text='Write your task: ')
+    created_at = DateTimeField(auto_now_add=True)
+    member_of_project = IntegerField(default=0)
+    date_line = CharField(max_length=100, help_text='Date line of your task (1 month): ')
+    user = ForeignKey('apps.User', CASCADE, related_name='projects')
