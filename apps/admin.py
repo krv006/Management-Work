6ae0a5cc7ca55f53cf50ac34141d1367734048ce
krv006 <rvkamronbek@gmail.com>
2025-04-14 12:26:46 +0500

    change admin and serializer

--- apps/admin.py
+++ apps/admin.py
@@ -16,26 +16,26 @@ class UserAdmin(ModelAdmin):
 # from apps.models import User, Project, Task
 #
 #
-# # @admin.register(User)
-# # class UserAdmin(ModelAdmin):
-# #     list_display = 'username', 'role', 'user_type', 'created_at',
-# #     list_editable = 'role', 'user_type',
-# #     list_filter = 'role', 'user_type',
-# #
-# #
-# # @admin.register(Project)
-# # class ProjectAdmin(admin.ModelAdmin):
-# #     list_display = "name", "manager", "manager", "display_team_members", "created_at",
-# #     list_editable = "manager",
-# #
-# #     def display_team_members(self, obj):
-# #         return ", ".join([user.username for user in obj.team_members.all()])
-# #
-# #     display_team_members.short_description = "Team Members"
-# #
-# #
-# # @admin.register(Task)
-# # class TaskAdmin(ModelAdmin):
-# #     list_display = 'project', 'title', 'assigned_to', 'status', 'created_at',
-# #     list_editable = 'status', 'assigned_to',
-# #     list_filter = 'title',
+# @admin.register(User)
+# class UserAdmin(ModelAdmin):
+#     list_display = 'username', 'role', 'user_type', 'created_at',
+#     list_editable = 'role', 'user_type',
+#     list_filter = 'role', 'user_type',
+#
+#
+# @admin.register(Project)
+# class ProjectAdmin(admin.ModelAdmin):
+#     list_display = "name", "manager", "manager", "display_team_members", "created_at",
+#     list_editable = "manager",
+#
+#     def display_team_members(self, obj):
+#         return ", ".join([user.username for user in obj.team_members.all()])
+#
+#     display_team_members.short_description = "Team Members"
+#
+#
+# @admin.register(Task)
+# class TaskAdmin(ModelAdmin):
+#     list_display = 'project', 'title', 'assigned_to', 'status', 'created_at',
+#     list_editable = 'status', 'assigned_to',
+#     list_filter = 'title',
