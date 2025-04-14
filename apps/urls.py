73a651de5f6ac73df21fad6cd06f9d3631c10174
krv006 <rvkamronbek@gmail.com>
2025-04-14 16:52:22 +0500

    change

--- apps/urls.py
+++ apps/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-
 from apps.views import LoginAPIView, UserRegisterCreateView, UserListAPIView
-from apps.views.site_view import SiteListCreateAPIView
+from apps.views.site_view import SiteListCreateAPIView, QuestionListCreateAPIView, AnswerListCreateAPIView
 
 urlpatterns = [
     path('register/', UserRegisterCreateView.as_view(), name='register'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('user/', UserListAPIView.as_view(), name='user-list'),
     path('site/', SiteListCreateAPIView.as_view(), name='site'),
+    path('question/', QuestionListCreateAPIView.as_view(), name='question'),
+    path('answer/', AnswerListCreateAPIView.as_view(), name='answer'),
 ]
--- apps/views/site_view.py
+++ apps/views/site_view.py
@@ -2,7 +2,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from apps.models import SiteSettings
+from apps.models import SiteSettings, Question, Answer
 from apps.serializers import SiteModelSerializer, QuestionModelSerializer, AnswerModelSerializer
 
 
@@ -10,5 +10,18 @@ from apps.serializers import SiteModelSerializer, QuestionModelSerializer, Answe
 class SiteListCreateAPIView(ListCreateAPIView):
     queryset = SiteSettings.objects.all()
     serializer_class = SiteModelSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = AllowAny,
+
+
+@extend_schema(tags=['SiteSettings'])
+class QuestionListCreateAPIView(ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionModelSerializer
+    permission_classes = AllowAny,
+
+
+@extend_schema(tags=['SiteSettings'])
+class AnswerListCreateAPIView(ListCreateAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerModelSerializer
     permission_classes = AllowAny,
