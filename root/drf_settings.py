902272e0916d06b6a7f6a937f45232e2f4c8aaa6
krv006 <rvkamronbek@gmail.com>
2025-04-17 19:51:37 +0500

    drf settings

--- root/drf_settings.py
+++ root/drf_settings.py
@@ -8,6 +8,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
 SPECTACULAR_SETTINGS = {
     'TITLE': 'API Tasks',
     'DESCRIPTION': 'API Tasks Description',
