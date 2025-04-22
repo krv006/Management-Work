ffb5d4654f7724f263cf7798ac616b42bdd75209
krv006 <rvkamronbek@gmail.com>
2025-04-22 11:13:29 +0500

    id add

--- apps/serializers/site_serializer.py
+++ apps/serializers/site_serializer.py
@@ -6,13 +6,13 @@ from apps.models import Question, Answer, SiteSettings
 class SiteModelSerializer(ModelSerializer):
     class Meta:
         model = SiteSettings
-        fields = 'region_to_region',
+        fields = 'id', 'region_to_region',
 
 
 class QuestionModelSerializer(ModelSerializer):
     class Meta:
         model = Question
-        fields = 'text', 'created_at',
+        fields = 'id', 'text', 'created_at',
 
     extra_kwargs = {
         'created_at': {'read_only': True},
@@ -22,7 +22,7 @@ class QuestionModelSerializer(ModelSerializer):
 class AnswerModelSerializer(ModelSerializer):
     class Meta:
         model = Answer
-        fields = 'text', 'question', 'created_at',
+        fields = 'id', 'text', 'question', 'created_at',
 
     extra_kwargs = {
         'created_at': {'read_only': True},
